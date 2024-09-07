6d5169ee2ed7c765ed48fc3e3b967474a7b9f512
김민규 <tjfvndfud@gmail.com>
2024-09-07 21:30:50 +0900

    feat: TodoList Create 기능 적용 - axios 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import axios from "axios";
 import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [todoItem, setTodoItem] = useState("");
+
+  const fetchTodos = async () => {
+    // axios를 사용하여 json-server의 todos를 가져옴
+    const response = await axios.get("http://localhost:4000/todos");
+    return response.data;
+  }
+  
+  const addTodo = async (newTodo) => {
+    await axios.post("http://localhost:4000/todos", newTodo)
+  }
+
+
+  /* 
+   * [1] useQuery는 두 개의 인자를 받음 
+   * 리액트 쿼리가 자동으로 데이터의 종류를 queryKey로 캐싱함
+   * useQuery로 호출한 데이터를 app.jsx에서쓰고있지만 A,B,C 컴포넌트에서 불러와도 DB까지안가고 캐싱되어 사용함
+   * 리액트 쿼리에서 useQuery를 쓰는 중요한 개념임
+  */
+
+  const { data: todos , isPending, isError } = useQuery({      
+      queryKey: ["todos"], // 배열로 받음
+      queryFn:fetchTodos // 비동기 함수
+  });
+
+  const mutation = useMutation({
+    mutationFn: addTodo,
+  });
+
+
+
+  if(isPending) {
+    return <div>로딩중입니다...</div>
+  }
+
+  if(isError) {
+    return <div>데이터 조회 중 오류가 발생했습니다...</div>
+  }
+
+  console.log("data", todos);
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tanstack Query</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+
+        // useMutation 로직 필요
+        mutation.mutate({
+          title: todoItem,
+          isDone: false
+        })
+      }}>
+        <input 
+          type="text" 
+          value={todoItem}
+          onChange={(e) => setTodoItem(e.target.value)}
+        />
+        <button>추가</button>
+        <ul>
+          {
+            todos.map(todo => {
+              return (
+                <li 
+                  key={todo.id} 
+                  style={{ 
+                    display: "flex", 
+                    alignItems: "center",
+                    gap: "10px",                  
+                  }}
+                >
+                  <h4>{todo.title}</h4>
+                  <p>{todo.isDone ? "Done" : "Not Done"}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </form>      
     </>
   )
 }
